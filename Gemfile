f5b1208665fb8b0ea603d94d4d47f4b79f05412f
Brian Small <briansmall@brians-air.attlocal.net>
2018-09-07 12:14:11 -0500

    functionality complete

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'bulma', '~> 0.1.0'
+gem 'simple_form', '~> 4.0', '>= 4.0.1'
+
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
@@ -44,12 +48,15 @@ group :development, :test do
 end
 
 group :development do
+  gem 'guard', '~> 2.14', '>= 2.14.2'
+  gem 'guard-livereload', '~> 2.5', '>= 2.5.2', require: false
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'better_errors', '~> 2.5'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,67 @@
 class PostsController < ApplicationController
   def index
     # Return all `Post`
+    @posts = if params[:term]
+      Post.where('title LIKE ?', "%#{params[:term]}%") 
+    else
+      @posts = Post.all.order("created_at DESC")
+    end
   end
 
   def new
     # Return view to create a new Post
+    @post = Post.new
   end
 
   def create
     # Add a new `Post` to the database
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def destroy
     # Remove a `Post` from the database
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
+
+  def show
+    @post = Post.find(params[:id]);
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def search_params
+    params.require(:post).permit(:title, :body)
+  end 
+
+
 end
